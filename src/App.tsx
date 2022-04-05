75c7a712b64ebeb4fa22bf2a3a67469c43a1c0b4
Elkhwaga <mohamedelkhwaga9@gmail.com>
2022-04-05 19:38:32 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome
+          Hello React form ci
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          thanks
         </a>
       </header>
     </div>
