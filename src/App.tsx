c0a452b0c425def9981959b5cf4edb85ae6bfd36
Elkhwaga <mohamedelkhwaga9@gmail.com>
2022-04-06 19:35:43 +0200

    change netfliy

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>Hello React form ci</p>
+				<p>Hello React form Netfliy</p>
 				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
 					thanks
 				</a>
